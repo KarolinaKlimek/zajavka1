8e64c037f598b72d74dbc80fc62a95917553a77d
Karola <xx@gmail.com>
2023-04-27 21:12:51 +0200

    First commit - adding Person class

---
+++ Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("How are you doin'");
+        int a = 9;
+        System.out.println("Maybe this time it's gonna work... Hopes!");
+    }
+}
